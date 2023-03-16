62b6188a93dac80c20776193ddbaf76575f4c729
alsah <aplexidr@gmail.com>
2023-03-16 21:12:39 +0200

    repo(cmake): Add 3d-party libs as external projects

---
+++ cmake/manage-3d-parties.cmake
@@ -0,0 +1,136 @@
+
+
+include(ExternalProject)
+
+
+macro(get_libdatachannel_project)
+
+    message(STATUS "Configuring external libdatachannel project ... ")
+
+    set(LIBDATACHANNEL_PROJECT  "${CMAKE_CURRENT_BINARY_DIR}/libdatachannel")
+    set(LIBDATACHANNEL_INCLUDES "${LIBDATACHANNEL_PROJECT}/res/include")
+    set(LIBDATACHANNEL_BINARIES "${LIBDATACHANNEL_PROJECT}/res/lib")
+    set(LIBDATACHANNEL_LIB_NAME "${CMAKE_SHARED_LIBRARY_PREFIX}datachannel${CMAKE_SHARED_LIBRARY_SUFFIX}")
+
+    set(LIBDATACHANNEL_CMAKE_ARGS
+            # environment
+            -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+            -DCMAKE_GENERATOR=${CMAKE_GENERATOR}
+            -DCMAKE_MAKE_PROGRAM=${CMAKE_MAKE_PROGRAM}
+            -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR> # install in cmake-build folder
+
+            # project (libdatachannel) specific options
+            -DNO_EXAMPLES:BOOL=ON
+            -DNO_TESTS:BOOL=ON
+            )
+
+
+    ExternalProject_Add(sd-party-libdatachannel
+
+            # directory options
+            PREFIX "${LIBDATACHANNEL_PROJECT}" # root directory for the external (libdatachannel) project
+            STAMP_DIR       "${LIBDATACHANNEL_PROJECT}/stamp"
+            SOURCE_DIR      "${LIBDATACHANNEL_PROJECT}/src"
+            BINARY_DIR      "${LIBDATACHANNEL_PROJECT}/bin"
+            INSTALL_DIR     "${LIBDATACHANNEL_PROJECT}/res"
+            LOG_DIR         "${LIBDATACHANNEL_PROJECT}/log"
+
+            # Download step (using git)
+            GIT_REPOSITORY  "https://github.com/paullouisageneau/libdatachannel"
+            GIT_REMOTE_NAME "origin"
+            GIT_TAG         "refs/tags/v0.18.2" #376a59eaed4b93fb7851524819ad3eb71c6e52af
+            # GIT_SUBMODULES ??
+            GIT_SUBMODULES_RECURSE ON
+            GIT_PROGRESS    ON
+            GIT_SHALLOW     ON  # Get just specified commit (GIT_TAG)
+
+            # Configure step
+            #CMAKE_GENERATOR: the same generator as the main build
+            CMAKE_ARGS          "${LIBDATACHANNEL_CMAKE_ARGS}"
+
+            UPDATE_COMMAND ""
+
+            #Build step (BUILD_BYPRODUCTS must be specified when 'ninja' is used)
+            # TODO some strange magic happened here; I want to build target datachannel-static
+            #  default build command is: "cmake;--build;." with ';' ???
+            #  it fails with "${CMAKE_COMMAND} --build . --target datachannel-static"
+            # BUILD_COMMAND       "${CMAKE_COMMAND};--build;.;--target datachannel-static" - passed
+            # TODO: Use static version of this library
+            BUILD_BYPRODUCTS    "${LIBDATACHANNEL_BINARIES}/${LIBDATACHANNEL_LIB_NAME}"
+            # https://stackoverflow.com/questions/54866067/cmake-and-ninja-missing-and-no-known-rule-to-make-it
+
+            # Test step
+            TEST_COMMAND "" # Run without tests
+
+            # Log
+            LOG_DOWNLOAD    ON
+            LOG_CONFIGURE   ON
+            LOG_BUILD       ON
+            LOG_INSTALL     ON
+            )
+
+    #add_library(libdatachannel SHARED IMPORTED)
+    add_library(libdatachannel SHARED IMPORTED)
+
+    #set_target_properties(libdatachannel PROPERTIES IMPORTED_LOCATION ${LIBDATACHANNEL_BINARIES}/libdatachannel.so)
+    # This project does not provide install option for the static library, so use dynamic (fix it in future)
+    set_target_properties(libdatachannel PROPERTIES
+            IMPORTED_LOCATION ${LIBDATACHANNEL_BINARIES}/${LIBDATACHANNEL_LIB_NAME}
+            )
+
+endmacro()
+
+
+
+macro(get_nlohmann_json_project)
+
+    message(STATUS "Configuring external nlohmann_json project ... ")
+
+    set(NLOHMANN_JSON_PROJECT   "${CMAKE_CURRENT_BINARY_DIR}/nlohmann-json")
+    set(NLOHMANN_JSON_INCLUDES  "${NLOHMANN_JSON_PROJECT}/res/include")
+
+    set(NLOHMANN_JSON_CMAKE_ARGS
+            # environment
+            -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+            -DCMAKE_GENERATOR=${CMAKE_GENERATOR}
+            -DCMAKE_MAKE_PROGRAM=${CMAKE_MAKE_PROGRAM}
+            -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR> # install in cmake-build folder
+
+            # project (nlohmann-json) specific options
+            # ???
+            )
+
+    # Configure nlohmann json as an external project
+    ExternalProject_Add(sd-party-nlohmann-json
+
+            # directory options
+            PREFIX "${NLOHMANN_JSON_PROJECT}" # root directory for the external (nlohmann-json) project
+            STAMP_DIR       "${NLOHMANN_JSON_PROJECT}/stamp"
+            SOURCE_DIR      "${NLOHMANN_JSON_PROJECT}/src"
+            BINARY_DIR      "${NLOHMANN_JSON_PROJECT}/bin"
+            INSTALL_DIR     "${NLOHMANN_JSON_PROJECT}/res"
+            LOG_DIR         "${NLOHMANN_JSON_PROJECT}/log"
+
+            GIT_REPOSITORY  "https://github.com/nlohmann/json.git"
+            GIT_REMOTE_NAME "origin"
+            GIT_TAG         "refs/tags/v3.10.4"
+            GIT_PROGRESS    ON
+            GIT_SHALLOW     ON  # Get just specified commit (GIT_TAG)
+
+            CMAKE_ARGS      "${NLOHMANN_JSON_CMAKE_ARGS}"
+
+            UPDATE_COMMAND ""
+
+            # Test step
+            TEST_COMMAND "" # Run without tests
+
+            # This library is header only
+
+            # Log
+            LOG_DOWNLOAD    ON
+            LOG_CONFIGURE   ON
+            LOG_BUILD       ON
+            LOG_INSTALL     ON
+            )
+
+endmacro()
